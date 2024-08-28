d2bb7ec5113acd28c80c78e3daae61d61242ec21
Acácia de Lima <86326134+cahlima@users.noreply.github.com>
2024-08-28 10:09:07 -0300

    Update matriculadas.blade.php

--- resources/views/professor/atividades/matriculadas.blade.php
+++ resources/views/professor/atividades/matriculadas.blade.php
@@ -1,48 +1,11 @@
-<!-- resources/views/professor/atividades/matriculadas.blade.php -->
-@extends('layouts.base')
+@extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <!-- Sidebar -->
-        <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar fixed">
-            <div class="position-sticky">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('professor.painel') }}">
-                            {{ __('Voltar') }}
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('atividades.listarPublicas') }}">
-                            {{ __('Atividades Disponíveis') }}
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('professor.atividades.matriculadas') }}">
-                            {{ __('Minhas Atividades') }}
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('professor.perfil.edit') }}">
-                            {{ __('Meu Perfil') }}
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            {{ __('Sair') }}
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-
-        <!-- Main content -->
-        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+<div class="container">
+<div class="row justify-content-center">
+     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <h2>{{ __('Minhas Atividades Matriculadas') }}</h2>
+
 
             @if($atividades->isNotEmpty())
                 <div class="table-responsive mt-4">
